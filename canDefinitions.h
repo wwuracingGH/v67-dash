4cf71f59103589ec2357e92aa33560f117424907
Nicole Swierstra <swiersw@wwu.edu>
2026-01-20 17:40:32 -0800

    appended can definitions

--- canDefinitions.h
+++ canDefinitions.h
@@ -3,11 +3,12 @@
 
 /*
  * DEFINITIONS for motor controller usage as defined in this pdf:
- * https://app.box.com/s/vf9259qlaadhzxqiqrt5cco8xpsn84hk/file/27334613044
+ * https://www.cascadiamotion.com/_files/ugd/b5e2cb_e731a297509a4b2786cea7ef36199b7f.pdf
  */ 
 
 #define MC_CANSTRUCT typedef struct __attribute__((packed, scalar_storage_order("little-endian")))
 #define VCU_CANSTRUCT typedef struct __attribute__((packed, scalar_storage_order("little-endian")))
+#define BMS_CANSTRUCT typedef struct __attribute__((packed, scalar_storage_order("little-endian")))
 
 typedef int16_t     MC_Temperature;
 typedef int16_t     MC_LowVoltage;
@@ -30,16 +31,18 @@ typedef int16_t     MC_Int;
 typedef uint16_t    MC_UnsignedInt;
 typedef uint8_t     MC_Byte;
 
-#define VCU_CANID_APPS_RAW                 0x001
-#define VCU_CANID_CALIBRATION              0x002
-#define VCU_CANID_WHEELSPEED               0x003
-#define VCU_CANID_ACCEL                    0x004
-#define VCU_CANID_STATE                    0x005
-#define VCU_CANID_REPROGRAMAPPS            0x006
-#define VCU_CANID_REPROGRAMCONTROL         0x007
-#define VCU_CANID_REVEAL_VALS              0x008
-#define VCU_CANID_APPS_VALS                0x009
-#define VCU_CANID_CONTROL_VALS             0x00A
+#define DL_CANID_WHEELSPEED                0x001
+#define DL_CANID_ACCEL                     0x002
+
+#define VCU_CANID_APPS_RAW                 0x101
+#define VCU_CANID_BPS_RAW              	   0x102
+#define VCU_CANID_CTRL_VEC                 0x103
+#define VCU_CANID_VCU_STATE            	   0x104
+#define VCU_CANID_PARAM_REVEAL             0x105
+#define VCU_CANID_PARAM_CHANGE             0x106
+#define VCU_CANID_PARAM_REQUEST            0x107
+
+#define BMS_CANID_IMPORTANT_PARAMS         0x080
 
 #define MC_CANID_TEMPERATURE1              0x0A0
 #define MC_CANID_TEMPERATURE2              0x0A1
@@ -426,18 +429,47 @@ VCU_CANSTRUCT {
 VCU_CANSTRUCT {
     uint16_t carAccel_X;
     uint16_t carAccel_Y;
+    uint16_t yawRate;
 } DL_CarAcceleration;
 
 VCU_CANSTRUCT {
-    uint16_t new_APPS1_MIN;
-    uint16_t new_APPS1_MAX;
-    uint16_t new_APPS2_MIN;
-    uint16_t new_APPS2_MAX;
-} VCU_ReprogramApps;
+	uint16_t apps1;
+	uint16_t apps2;
+	uint16_t apps3;
+	uint16_t apps4;
+} VCU_AppsRaw;
 
 VCU_CANSTRUCT {
-    uint16_t new_MaxTorqueReq;
-    uint16_t new_MinRegenReq;
-    uint16_t new_MinRegenSpeed;
-    uint16_t new_BrakeThreashold;
-} VCU_ReprogramControl;
+	uint16_t fbps;
+	uint16_t rbps;
+} VCU_BpsRaw;
+
+VCU_CANSTRUCT {
+	uint8_t state;
+	uint8_t faultCounter    : 7;
+    uint8_t pLatch          : 1;
+    uint16_t lastValidTorqueReq;
+} VCU_VCUState;
+
+VCU_CANSTRUCT {
+	uint32_t setValue;
+	uint16_t id;
+} VCU_ParamReveal;
+
+VCU_CANSTRUCT {
+	uint32_t setValue;
+	uint16_t id : 15;
+	uint16_t write : 1;
+} VCU_ParamSet;
+
+VCU_CANSTRUCT {
+	uint16_t id;
+} VCU_ParamReq;
+
+BMS_CANSTRUCT {
+    uint16_t pack_dischargeLimitKW;
+    uint16_t pack_openVoltage;
+    uint16_t reserved_1;
+    uint8_t pack_SOC;
+    uint8_t reserved_2;
+} BMS_important;
